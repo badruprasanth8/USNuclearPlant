82643dc4abfd9dd4fa5f9c81c7acefb7cb6b7247
Badru <badru8@hotmail.com>
2022-06-08 01:40:26 +0530

    Html updates

--- frontend/src/app/popup.service.ts
+++ frontend/src/app/popup.service.ts
@@ -10,7 +10,7 @@ export class PopupService {
     return `` +
       `<div>Plant Name: ${ data.PlantName }</div>` +
       `<div>State: ${ data.State }</div>` +
-      `<div>Total plants: ${ data.TotalPlantsinState }</div>` +
+      `<div>Total N-plants: ${ data.TotalPlantsinState }</div>` +
       `<div>Nuclear Power Contribution to State: ${ data.PercentForState } %</div>`+
       `<div>Absolute Nuclear Power capacity: ${ data.AbsoulutePower } (MWh) </div>`
   }
